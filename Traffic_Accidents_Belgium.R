74c9afd703f9ae344130f324f04a11cd9f68b07b
Andrea Gabriele <gabriele.a@pg.com>
2018-01-19 16:49:50 +0100

    test

--- Traffic_Accidents_Belgium.R
+++ Traffic_Accidents_Belgium.R
@@ -95,7 +95,7 @@ ggplot(df_reduce_summary, aes(x=df_reduce_summary$`year(DT_DAY)`,y=`sum(MS_DEAD)
         axis.text.y=element_text(size = 9, colour = "black", angle = 0, hjust = 0, vjust = 0.5)) +
 scale_fill_manual("Gender",values=c("orange","red","grey"))
 
-   
+    
 ### Difference between Male / Female Victimes
 df_reduce_summary <- df_reduce %>% 
   filter(TX_VCT_TYPE_DESCR_FR!= "Autre victime" & TX_VCT_TYPE_DESCR_FR!= "Non disponible") %>% 
